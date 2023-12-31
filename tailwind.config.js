019a642e56da3bdbc4371523990a5dfefde9b923
Legitgoons <cksxkr5193@naver.com>
2024-01-01 03:04:15 +0900

    Set: 사이트에 사용될 font, color 정의
    tailwind.config.js에 사용될 font와 color를 설정하였습니다.
    index.css에 사이트 내에서 사용할 font의 형식을 정의하여 쉽게 사용할 수 있도록 했습니다.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Primary: '#545F71',
+        Secondary: '#9BA5B7',
+        Tertiary: '#EEF1F4',
+        Skyblue: '#BADEFF',
+        Darkblue: '#186EBF',
+        Orange: '#FF844F',
+        Red: '#FF5361',
+      },
+      fontFamily: {
+        suit: ['SUIT', 'sans-serif'],
+      },
+      fontSize: {
+        20: '20px',
+        24: '24px',
+        28: '28px',
+        32: '32px',
+        40: '40px',
+        48: '48px',
+      },
+      lineHeight: {
+        120: '120%',
+      },
+      fontWeight: {
+        semibold: '600',
+        heavy: '900',
+      },
+    },
   },
   plugins: [],
 };
