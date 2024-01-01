f7eccfeae437a558015206ac277ea959a0f67125
Uichan Lee <cksxkr5193@naver.com>
2024-01-01 20:49:34 +0900

    Set: 절대경로 설정 및 @types/node 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@public', replacement: path.resolve(__dirname, 'public') },
+    ],
+  },
 });
