5caac0686164b42188728d04222e17ba07e3ee23
Uichan Lee <cksxkr5193@naver.com>
2024-01-02 21:16:05 +0900

    Fix: HeaderIcon의 style prop를 layout으로 변경
    상위 컴포넌트에서 layout을 조정하기 위해 만든 prop이기에 좀 더 직관적인 이름으로 변경하였습니다.

--- src/components/atoms/HeaderIcon.tsx
+++ src/components/atoms/HeaderIcon.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from 'react';
 
 interface HeaderIconProps {
   children: ReactNode;
-  styles?: string;
+  layout?: string;
 }
 
-export default function HeaderIcon({ children, styles }: HeaderIconProps) {
+export default function HeaderIcon({ children, layout }: HeaderIconProps) {
   return (
-    <div className={`${styles} w-12 md:w-20 flex items-center`}>{children}</div>
+    <div className={`${layout} w-12 md:w-20 flex items-center`}>{children}</div>
   );
 }
