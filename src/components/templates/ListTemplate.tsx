78da4cf3ad9e856e07c5322a71cb023bf4d2220e
Uichan Lee <cksxkr5193@naver.com>
2024-01-03 05:03:37 +0900

    Feat: layout 배치를 위한 ListTemplate 컴포넌트 구현

--- src/components/templates/ListTemplate.tsx
+++ src/components/templates/ListTemplate.tsx
@@ -1,10 +1,29 @@
-import Header from '@/components/organisms/Header';
+import { ListCardData } from '@/types/apis/listData';
 
-export default function ListTemplate() {
+import Header from '../organisms/Header';
+import ListCard from '../organisms/ListCard';
+
+interface ListTemplateProps {
+  data: ListCardData[];
+}
+
+export default function ListTemplate({ data }: ListTemplateProps) {
   return (
-    <div className="w-screen h-screen flex flex-col items-center">
+    <div className="w-screen h-screen flex flex-col items-center overflow-auto">
       <Header currentPage="list" />
-      <p>상품 리스트 페이지입니다.</p>
+      <ol className="container flex flex-wrap justify-center">
+        {data.map((item, index) => (
+          <li
+            key={item.id}
+            className="w-80 h-500 flex items-start justify-center py-8"
+          >
+            <div className="w-72 flex flex-col items-start">
+              <span className="t2">{String(index + 1).padStart(2, '0')}</span>
+              <ListCard data={item} />
+            </div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
