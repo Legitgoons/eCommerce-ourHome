41f1f97df025a7ddf6c595654414f1ac4f4d4bbf
Uichan Lee <cksxkr5193@naver.com>
2024-01-03 20:59:06 +0900

    Feat: FoodImageBox 이름 변경 및 재사용을 위한 size prop, 웹 접근성을 위한 name prop 추가

---
+++ src/components/atoms/FoodImageBox.tsx
@@ -0,0 +1,19 @@
+interface FoodImageBoxProps {
+  src: string;
+  name: string;
+  size?: 'lg' | 'sm';
+}
+
+export default function FoodImageBox({
+  src,
+  name,
+  size = 'lg',
+}: FoodImageBoxProps) {
+  const boxSize = size === 'lg' ? 'w-72 h-72' : 'w-32 h-32';
+
+  return (
+    <div className={`${boxSize} flex item-center justify-center border-2`}>
+      <img src={src} alt={name} />
+    </div>
+  );
+}
