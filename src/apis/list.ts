c0beeec92ff4ded7aaa1eeb1f457bd65823e9115
Uichan Lee <cksxkr5193@naver.com>
2024-01-16 00:53:36 +0900

    Fix: json-server 이용을 위한 fetchListData 수정

--- src/apis/list.ts
+++ src/apis/list.ts
@@ -1,7 +1,16 @@
 import { ListCardData } from '@/types/apis/listData';
 
 export const fetchListData = async (): Promise<ListCardData[]> => {
-  const response = await fetch('/data/listData.json');
-  const data: ListCardData[] = await response.json();
-  return data;
+  try {
+    const response = await fetch('http://localhost:4000/products');
+
+    if (!response.ok) {
+      throw new Error('Server responded with an error');
+    }
+    const data: ListCardData[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
